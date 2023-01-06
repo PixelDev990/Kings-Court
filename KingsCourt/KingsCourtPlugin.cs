295b027d99a9c859352f51c44d18323e5a93b114
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-01-06 20:19:55 +0530

    Update KingsCourtPlugin.cs

--- KingsCourt/KingsCourtPlugin.cs
+++ KingsCourt/KingsCourtPlugin.cs
@@ -1,59 +1,33 @@
-﻿using BepInEx;
+using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.IL2CPP;
-using Essentials.Options;
 using HarmonyLib;
-using PeasAPI;
-using PeasAPI.Components;
-using PeasAPI.Managers;
 using Reactor;
-using UnityEngine;
 
-namespace KingsCourt
+namespace KingsCourt;
+
+[BepInAutoPlugin]
+[BepInProcess("Among Us.exe")]
+[BepInDependency(ReactorPlugin.Id)]
+public partial class KingsCourtPlugin : BasePlugin
 {
-    [BepInPlugin(Id)]
-    [BepInProcess("Among Us.exe")]
-    [BepInDependency(ReactorPlugin.Id)]
-    [BepInDependency(PeasApi.Id)]
-    public class KingsCourtPlugin : BasePlugin
+    public Harmony Harmony { get; } = new(Id);
+
+    public ConfigEntry<string> ConfigName { get; private set; }
+
+    public override void Load()
     {
-        public const string Id = "pixeldev990.amongus.kingscourt";
+        ConfigName = Config.Bind("Fake", "Name", ":>");
 
-        public Harmony Harmony { get; } = new Harmony(Id);
+        Harmony.PatchAll();
+    }
 
-        public static CustomOptionHeader KingsCourtOptions = CustomOption.AddHeader("<color=#2294E6>KingsCourt Mod Options:</color>");
-
-        public static CustomOptionHeader Jester = CustomOption.AddHeader("<color=#ff00ff>Jester:</color>");
-        public static CustomNumberOption JesterAmount = CustomOption.AddNumber("JesterAmount", "Jester Amount", true, 1, 0, 3, 1);
-
-        public static CustomOptionHeader Trickster = CustomOption.AddHeader("<color=#d400ff>Trickster:</color>");
-        public static CustomNumberOption TricksterAmount = CustomOption.AddNumber("TricksterAmount", "Trickster Amount", true, 1, 0, 3, 1);
-
-        public static CustomOptionHeader Joker = CustomOption.AddHeader("<color=#00a6ff>Joker:</color>");
-        public static CustomNumberOption JokerAmount = CustomOption.AddNumber("JokerAmount", "Joker Amount", true, 1, 0, 3, 1);
-
-        public static CustomOptionHeader Troll = CustomOption.AddHeader("<color=#00ff00>Troll:</color>");
-        public static CustomNumberOption TrollAmount = CustomOption.AddNumber("TrollAmount", "Troll Amount", true, 1, 0, 3, 1);
-        public static CustomNumberOption ConfuseCooldown = CustomOption.AddNumber("ConfuseCooldown", "Confuse Cooldown", true, 50, 40, 60, 5);
-        public static CustomNumberOption MeetingCooldown = CustomOption.AddNumber("MeetingCooldown", "Meeting Cooldown", true, 50, 40, 60, 5);
-        public override void Load()
+    [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.FixedUpdate))]
+    public static class ExamplePatch
+    {
+        public static void Postfix(PlayerControl __instance)
         {
-            WatermarkManager.VersionText = "\n<color=#00FA9A>Kings Court</color> \n<color=#FFD11AFF>By MengTube, PlusFlippy & PixelKing</color> \n<color=#18d5b9>Button Art C.A.100</color>";
-            WatermarkManager.VersionTextOffset = new Vector3(0f, -0.65f, 0f);
-            WatermarkManager.PingText = "\nMade By <color=#FFD11AFF>MengTube, PlusFlippy & PixelKing</color> \n<color=#2eadff>dsc.gg/betterau</color>";
-            WatermarkManager.PingTextOffset = new Vector3(-1.4f, 0f, 0f);
-            PeasApi.AccountTabOffset = new Vector3(0f, -0.9f, 0f);
-            PeasApi.AccountTabOnlyChangesName = true;
-
-            CustomServerManager.RegisterServer("TestServer", "152.228.160.91", 22023);
-
-            CustomOption.ShamelessPlug = false;
-            CustomOption.HudTextScroller = false;
-
-            RegisterCustomRoleAttribute.Register(this);
-
-            Harmony.PatchAll();
+            __instance.nameText.text = PluginSingleton<KingsCourtPlugin>.Instance.ConfigName.Value;
         }
     }
 }
-
-
